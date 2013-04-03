dff574ddf77f6ad97fb2a4e3bda9647984e8398d
phyber.apex@gmail.com <phyber.apex@gmail.com@b506273b-8a10-26d3-516e-ea948422f817>
2013-04-03 09:15:04 +0000

    Patchnotes

--- Diary of Legends/src/de/phyberapex/diaryoflegends/model/Role.java
+++ Diary of Legends/src/de/phyberapex/diaryoflegends/model/Role.java
@@ -3,14 +3,16 @@ package de.phyberapex.diaryoflegends.model;
 import java.awt.Color;
 
 public enum Role {
-	TOP("Top-Lane", Color.ORANGE), SUPPORT("Support", Color.YELLOW), MID("Mid-Lane", Color.BLUE), JUNGLE("Jungle", Color.GREEN), AD_CARRY(
-			"Ad-Carry",Color.RED);
+	TOP("Top-Lane", Color.decode("#116178")), SUPPORT("Support", Color
+			.decode("#3f7811")), MID("Mid-Lane", Color.decode("#783611")), JUNGLE(
+			"Jungle", Color.decode("#552266")), AD_CARRY("Ad-Carry", Color
+			.decode("#771111"));
 
 	/**
 	 * String representation of the Role
 	 */
 	private String description;
-	private Color color;
+	transient private Color color;
 
 	private Role(String desc, Color rep) {
 		this.description = desc;
@@ -25,10 +27,10 @@ public enum Role {
 		return this.description;
 	}
 
-	public Color getColor(){
+	public Color getColor() {
 		return this.color;
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.description;
